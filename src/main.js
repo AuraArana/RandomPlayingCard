9802d5aa6740b4986284931cf42fb46429f362a9
Aura Arana <Aura.Arana001@mymdc.net>
2021-02-26 19:10:08 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -6,5 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let generateRandomNumber = () => {
+    let number = [
+      "A",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumber = Math.floor(Math.random() * number.length);
+    return number[indexNumber];
+  };
+
+  let generateRandomSymbol = () => {
+    let symbol = ["&hearts;", "&diams;", "&spades;", "&clubs;"];
+    let indexSymbol = Math.floor(Math.random() * symbol.length);
+    return symbol[indexSymbol];
+  };
+
+  document.querySelector(".top-symbol").innerHTML = generateRandomSymbol();
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  document.querySelector(".bottom-symbol").innerHTML = generateRandomSymbol();
+
+  //   console.log("Hello Rigo from the console!");
 };
